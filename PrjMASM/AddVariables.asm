00b32899e8f8cb735ed52cacacaf798663a095d0
Jianmin Wei <jianminwei1@gmail.com>
2020-06-28 08:36:51 -0400

    added Moves.asm

--- PrjMASM/AddVariables.asm
+++ PrjMASM/AddVariables.asm
@@ -15,12 +15,22 @@ secondval dword 11111111h
 thirdval  dword 22222222h
 sum dword 0
 
+byteVal BYTE 10001111b
+
 .code
 main proc
 	mov	eax,firstval				
 	add	eax,secondval		
 	add eax,thirdval
 	mov sum,eax
+
+	;below are for two useful instructions.
+	movzx eax, byteVal	; the movzx if for moving unsigned data
+						; from a smaller size (8, 16) to larger size (16, 32)
+	movsx eax, byteVal	;  movsx is for moving signed data
+
+	lahf				; copy FLAGS to AH
+	sahf				; save AH to FLAGS.
 
 	invoke ExitProcess,0
 main endp
